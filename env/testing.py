39c6f67bbe68b8a068fb54dcf2a7ab4690ec2005
duncan-hintz <122481322+duncan-hintz@users.noreply.github.com>
2026-03-11 10:10:15 -0700

    Added env folder

---
+++ env/testing.py
@@ -0,0 +1,26 @@
+from customEnv import CustomEnv
+
+def episode():
+    #pettingzoo env
+
+    parallel_env = CustomEnv(render_mode="human")
+    observations,info=parallel_env.reset()
+
+    while parallel_env.agents:
+        #policy
+        #Choose an action
+        actions = {}
+        for agent in parallel_env.agents:
+            #This uses the sample function, which is random with a mask
+            actions[agent]=parallel_env.action_space(agent).sample(mask=observations[agent]["action_mask"])
+            
+        observations,rewards, terminations, truncations, infos = parallel_env.step(actions)
+
+    for battle_tag, battle in parallel_env.agent1.battles.items():
+        print("Finished game ", battle_tag)
+    
+    parallel_env.close()
+
+if __name__ == "__main__":
+    #for i in range(10):
+        episode()
